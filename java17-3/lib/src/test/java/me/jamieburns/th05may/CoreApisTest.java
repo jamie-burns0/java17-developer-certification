69156e4f8edbc07de9e615e769decc44e1ac8a65
Jamie Burns <jamie_r_burns@hotmail.com>
2022-05-05 17:19:59 +1000

    added arrays test

--- java17-3/lib/src/test/java/me/jamieburns/th05may/CoreApisTest.java
+++ java17-3/lib/src/test/java/me/jamieburns/th05may/CoreApisTest.java
@@ -3,6 +3,8 @@ package me.jamieburns.th05may;
 import me.jamieburns.Test;
 import static me.jamieburns.TestSupport.tests;
 
+import java.util.Arrays;
+
 public class CoreApisTest {
 
     public static void main(String[] args) {
@@ -49,12 +51,108 @@ public class CoreApisTest {
                 var d = (hello == ("Hel"+lo).intern()); // true
                 return true;}, true),                
             new Test(() -> {
-                byte[] a = {(byte) 0xff, 0100, 0b0011};
+                byte[] a = {0x7f, 0100, 0b0011}; // 0x7f (127) is the largest positive integer that can fit into a byte.
+                                                 // A larger value needs to be case to byte: (byte)0x80
                 int b = a.length;
-                return a[2] == (byte)b;}, true),
-            new Test(() -> {return 0;}, 0),
-            new Test(() -> {return 0;}, 0),
-            new Test(() -> {return 0;}, 0),
+                return a[2] == b;}, true),
+            new Test(() -> {
+                char[] a = new char[3];
+                String b = "God";
+                String c = b.toString(); // b returns the reference to "God"
+                var d = (b == c);
+                return d;}, true),
+            new Test(() -> {
+                String a = "God";
+                char[] b = {'G', 'o', 'd'};
+                String c = new StringBuilder().append(a).toString();
+                var d = (a == c);
+                return d;}, false),
+            new Test(() -> {
+                String a = "God";
+                char[] b = {'G', 'o', 'd'};
+                String c = new String(b);
+                var d = (a == c);
+                return d;}, false),
+            new Test(() -> {
+                String[] a = {"in", "the", "beginning", "was", "God"};
+                String b[] = a;
+                var d = (a == b) && (a.equals(b)); // an array is an object, so we can call Object.equals(...)
+                return d;}, true),
+            new Test(() -> {
+                String[] a = {"in", "the", "beginning", "was", "God"};
+                String b[] = {"in", "the", "beginning", "was", "God"};
+                var d = (a == b) && (a.equals(b)); // a and b are different objects
+                return d;}, false),
+            new Test(() -> {
+                String[] a = {"in", null, "beginning", "was", "God"};
+                String b[] = (String[])a;
+                var d = (a.equals(b));
+                var sb = new StringBuilder();
+                for (String s : b) {
+                    if (s == null) {
+                        continue;
+                    }
+                    sb.append(s);
+                }
+                return sb.toString();}, "inbeginningwasGod"),
+            new Test(() -> {
+                String []a = {"in", "the", "beginning", null, "God"};
+                var sb = new StringBuffer();
+                for (int i = 0; i < a.length; i++) {
+                    if (a[i] == null) {
+                        break;
+                    }
+                    sb.append(a[i]);
+                }
+                return sb.toString();}, "inthebeginning"),
+            new Test(() -> {
+                String[] a = {"Psalm", "23", "The", "Lord", "is", "my", "shepherd"};
+                Arrays.sort(a);
+                var sb = new StringBuilder();
+                for (String s : a) {
+                    sb.append(s);
+                }
+                return sb.toString();}, "23LordPsalmTheismyshepherd"), // ASCII: numberals, uppercase, lowercase            
+            new Test(() -> {
+                var a = new String(new char[] {'G', '\u006f', '\u0064'});
+                return a;}, "God"),
+            new Test(() -> {
+                String[] a = {"Psalm", "23", "The", "Lord", "is", "my", "shepherd"};
+                var i = Arrays.binarySearch(a, "Lord"); // Our array is not in sort order, so the result here is unpredictable
+                Arrays.sort(a);
+                var i2 = Arrays.binarySearch(a, "Lord"); // 1, ie a[1]
+                var i3 = Arrays.binarySearch(a, "i"); // -5: not found but if it was in the array it would live immediately before "is", a[4]
+                return Integer.toString(i2) + Integer.toString(i3);}, "1-5"),
+            new Test(() -> {
+                String[] a = {"Psalm", "23", "The", "Lord", "is", "my", "shepherd", "I"};
+                String[] b = {"John", "1", "In", "the", "beginning", "was", "the", "Word"};
+                var i = Arrays.compare(a, b); // a +ve int as "Psalm" is larger than "John" ????
+                return i > 0;}, true), 
+            new Test(() -> {
+                Object[] a = {"a", "b", 21, "d", "d"};
+                Object[] b = {"a", "b", 21, "d", "e"};
+                var i = Arrays.mismatch(a, b); // mismatch() occurs on the a[4], b[4]
+                                               // mismatch() does NOT sort the array under the hood. It uses the order provided,
+                                               // comparing a[0] with b[0], a[1] with b[1], and so on.
+                return i;}, 4),
+            //
+            // For [].equals(), Arrays.compare() and Arrays.mismatch()
+            //
+            // Method     | array contains | arrays are
+            //            | same data      | different
+            // -----------+----------------+------------------
+            // equals()   | true           | false
+            // compare()  | 0              | +ve or -ve number
+            // mismatch() | -1             | zero or +ve index
+            //
+            new Test(() -> {
+                int[] a = {1, 2, 3, 4, 5, 6};        // An array object holding ints
+                int[][] b = {{1,2}, {3}, {4, 5, 6}}; // An array object holding array objects holding ints. Each array in our array can be a different size.
+                int[][] c = new int[3][];
+                b[0] = new int[] {1,2};
+                b[1] = new int[] {3};
+                b[2] = new int[] {4, 5, 6};
+                return b.equals(c);}, true), // WHY IS THIS FALSE??
             new Test(() -> {return 0;}, 0),
             new Test(() -> {return 0;}, 0),
             new Test(() -> {return 0;}, 0),
