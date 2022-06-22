9698562f5f2722e0da4b09ed165d0cf5ffbfc1c2
Jamie Burns <jamie_r_burns@hotmail.com>
2022-06-22 15:03:58 +1000

    refactor test framework

---
+++ java17-streams/lib/src/test/java/me/jamieburns/streams/Tester.java
@@ -0,0 +1,5 @@
+package me.jamieburns.streams;
+
+public interface Tester<T> {
+    public T test();
+}
---
+++ java17-streams/lib/src/test/java/me/jamieburns/streams/we22jun/OptionalTest.java
@@ -0,0 +1,32 @@
+package me.jamieburns.streams.we22jun;
+
+import static me.jamieburns.streams.TestSupport.tests;
+
+import me.jamieburns.streams.Test;
+
+public class OptionalTest {
+    
+public static void main(String[] args) {
+        new OptionalTest().testsToRun();
+    }
+
+    @org.testng.annotations.Test
+    public void testsToRun() {
+
+        tests(
+            new Test<Long>(() -> {
+                return 1L;}, Long.valueOf("1")),
+            new Test<Exception>(() -> {
+                return new Exception();}, new Exception(), (Exception a, Exception b) -> a.getClass().equals(b.getClass())),
+            new Test<String>(() -> {return "";}, ""),
+            new Test<>(() -> {return 0;}, 0),
+            new Test<>(() -> {return 0;}, 0),
+            new Test<>(() -> {return 0;}, 0),                
+            new Test<>(() -> {return 0;}, 0),
+            new Test<>(() -> {return 0;}, 0),
+            new Test<>(() -> {return 0;}, 0),
+            new Test<>(() -> {return 0;}, 0),
+            new Test<>(() -> {return 0;}, 0)
+        );
+    }
+}
