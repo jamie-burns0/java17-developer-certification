27e19977d724af07721104a95aacae58a26612a5
Jamie Burns <jamie_r_burns@hotmail.com>
2022-06-29 18:02:17 +1000

    cleaned up unused imports

--- java17-streams/lib/src/test/java/me/jamieburns/streams/day3/StreamsTest.java
+++ java17-streams/lib/src/test/java/me/jamieburns/streams/day3/StreamsTest.java
@@ -4,12 +4,8 @@ import static me.jamieburns.streams.TestSupport.tests;
 
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.Map;
-import java.util.HashMap;
 
 import me.jamieburns.streams.Test;
 
