46b49507580759e1cfa5232aa7a28e397d48ccc0
jamie-burns0 <104957268+jamie-burns0@users.noreply.github.com>
2022-07-02 21:47:34 +1000

    add day 6 tests - collect, reduce

--- java17-streams/lib/src/test/java/me/jamieburns/streams/day6/StreamsTest.java
+++ java17-streams/lib/src/test/java/me/jamieburns/streams/day6/StreamsTest.java
@@ -25,16 +25,48 @@ public class StreamsTest {
                         System.out.println("CollectDebug::combine: " + this + ", " + that);
                     }
                 }
+
                 CollectDebug<Double> result = Stream.<Double>generate(Math::random)
+                    .parallel()
                     .limit(4)
+                    //.parallel()
                     .collect(// Supplier<R>, BiConsumer<R,T>, BiConsumer<R,R> : R
                         CollectDebug::new,
-                        (r, t) -> r.collect(t),
-                        (r1, r2) -> r1.combine(r2)
+                        //() -> new CollectDebug<Double>(),
+                        CollectDebug::collect,
+                        //(r, t) -> r.collect(t),
+                        CollectDebug::combine
+                        //(r1, r2) -> r1.combine(r2)                        
                     );
+
                 result.print();
                 return 0;}, 0),
-            new Test<>(() -> {return 0;}, 0),
+            new Test<>(() -> {
+                class ReduceDebug<T> extends Debug {
+                    ReduceDebug<T> reduce(T v) {
+                        System.out.println("ReduceDebug::reduce: " + this + ", " + v);
+                        return this;
+                    }
+                    ReduceDebug<T> combine(ReduceDebug<? extends T> that) {
+                        System.out.println("ReduceDebug::combine: " + this + ", " + that);
+                        return this;
+                    }
+                }
+
+                ReduceDebug<Double> result = Stream.generate(Math::random)
+                    //.parallel()
+                    .limit(4)
+                    //.parallel()
+                    .reduce(// U, BiFunction<U,T,U>, BinaryOperator<U> : U
+                        new ReduceDebug<Double>(),
+                        ReduceDebug::reduce,
+                        //(r, t) -> r.reduce(t),
+                        ReduceDebug::combine
+                        //(r1, r2) -> r1.combine(r2)
+                     );
+
+                result.print();
+                return 0;}, 0),
             new Test<>(() -> {return 0;}, 0),
             new Test<>(() -> {return 0;}, 0),
             new Test<>(() -> {return 0;}, 0),
