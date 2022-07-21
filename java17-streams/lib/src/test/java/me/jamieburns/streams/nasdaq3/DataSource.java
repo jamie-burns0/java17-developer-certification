5f7922ef5486ed768ac0850eea508ff1dac4bdbd
jamie-burns0 <104957268+jamie-burns0@users.noreply.github.com>
2022-07-21 18:35:24 +1000

    with parallel

--- java17-streams/lib/src/test/java/me/jamieburns/streams/nasdaq3/DataSource.java
+++ java17-streams/lib/src/test/java/me/jamieburns/streams/nasdaq3/DataSource.java
@@ -7,6 +7,8 @@ public class DataSource {
         "TRADE, BHP, 100.0", "TRADE, AMP, 64.5", "TRADE, BHP, 102.0",
         "CANCEL, BHP, 100.0", "TRADE, AMP, 64.5", "TRADE, BHP, 101.4",
         "TRADE, BHP, 101.5", "CANCEL, AMP, 64.5", "TRADE, BHP, 102.0",
-        "TRADE, BHP, 101.5"
+        "TRADE, BHP, 101.5", "TRADE, AMP, 64.5", "TRADE, BHP, 102.5",
+        "TRADE, AMP, 64.5", "TRADE, BHP, 101.4", "TRADE, BHP, 101.5",
+        "CANCEL, AMP, 64.5", "TRADE, BHP, 102.0", "TRADE, BHP, 101.5"
     );
 }
--- java17-streams/lib/src/test/java/me/jamieburns/streams/nasdaq3/TransactionDataCombiner.java
+++ java17-streams/lib/src/test/java/me/jamieburns/streams/nasdaq3/TransactionDataCombiner.java
@@ -1,23 +1,54 @@
 package me.jamieburns.streams.nasdaq3;
 
 import java.util.Map;
-import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
 
-public class TransactionDataCombiner implements BiConsumer<Map<String, Map<Double, Long>>, Map<String, Map<Double, Long>>> {
+public class TransactionDataCombiner implements BinaryOperator<Map<String, Map<Double, Long>>> {
     @Override
-    public void accept(Map<String, Map<Double, Long>> stockMap1, Map<String, Map<Double, Long>> stockMap2) {
-        
-        System.out.println("--- stockMap1 ---");
-        for (var es1 : stockMap1.entrySet()) {
-            for (var es2 : es1.getValue().entrySet()) {
-                System.out.println("[" + es1.getKey() + ", " + es2.getKey() + ", " + es2.getValue() + "]");
+    public Map<String, Map<Double, Long>> apply(Map<String, Map<Double, Long>> leftStockMap, Map<String, Map<Double, Long>> rightStockMap) {
+        /*
+        System.out.println("--- leftMap ---");
+        for (var leftStock : leftStockMap.entrySet()) {
+            for (var leftPrice : leftStock.getValue().entrySet()) {
+                System.out.println("[" + leftStock.getKey() + ", " + leftPrice.getKey() + ", " + leftPrice.getValue() + "]");
             }
         }
-        System.out.println("--- stockMap2 ---");
-        for (var es1 : stockMap2.entrySet()) {
-            for (var es2 : es1.getValue().entrySet()) {
-                System.out.println("[" + es1.getKey() + ", " + es2.getKey() + ", " + es2.getValue() + "]");
+        System.out.println("--- rightMap ---");
+        for (var rightStock : rightStockMap.entrySet()) {
+            for (var rightPrice : rightStock.getValue().entrySet()) {
+                System.out.println("[" + rightStock.getKey() + ", " + rightPrice.getKey() + ", " + rightPrice.getValue() + "]");
             }
         }
-    }    
+        */
+        for (var rightStock : rightStockMap.entrySet()) { // each stock
+            if (leftStockMap.containsKey(rightStock.getKey()) == false) {
+                leftStockMap.put(rightStock.getKey(), rightStock.getValue());
+                continue;
+            }
+            // if we got to here, leftStockMap contains our stock
+            Map<Double, Long> leftPriceMap = leftStockMap.get(rightStock.getKey());
+            for (var rightPrice : rightStock.getValue().entrySet()) { // each price
+                if (leftPriceMap.containsKey(rightPrice.getKey()) == false) {
+                    leftPriceMap.put(rightPrice.getKey(), rightPrice.getValue());
+                    continue;
+                }
+                // if we got to here, leftPriceMap contains our price
+                long leftCount = leftPriceMap.get(rightPrice.getKey());
+                long rightCount = rightPrice.getValue();
+
+                leftCount += rightCount;
+
+                // if our price count is now 0 (zero), let's remove it from the map,
+                // otherwise, let's update it in our left map
+
+                if (leftCount == 0) {
+                    leftPriceMap.remove(rightPrice.getKey());
+                } else {
+                    leftPriceMap.put(rightPrice.getKey(), leftCount);
+                }                
+            }
+        }
+
+        return leftStockMap;
+    } 
 }
